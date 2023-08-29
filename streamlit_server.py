110938be17cc3d8a8418ffd1945e01eaeaf30fdc
KINGS0FA <97674213+KINGS0FA@users.noreply.github.com>
2023-08-29 10:26:56 +0800

    webbrowser alternative

--- streamlit_server.py
+++ streamlit_server.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import webbrowser
 
 st.title('Osu Link Converter')
 
-link = st.text_input('Enter Osu Link to convert',
-                     help='Enter Link ex:[https://osu.ppy.sh/beatmapsets/xxxxxx#osu/xxxxxxx')
+link = st.text_input('Enter Osu Link to convert', help='Enter Link ex:[https://osu.ppy.sh/beatmapsets/xxxxxx#osu/xxxxxxx')
 
 if st.button('Nerinyan'):
     start_marker = "beatmapsets/"
@@ -12,16 +10,15 @@ if st.button('Nerinyan'):
     start_index = link.find(start_marker) + len(start_marker)
     end_index = link.find(end_marker)
     extracted_string = link[start_index:end_index]
-
+    
     if len(link) == 0:
         st.markdown('<span style="color: red;">Enter URL!</span>', unsafe_allow_html=True)
     elif 'beatmapsets/' not in link:
         st.markdown('<span style="color: red;">Invalid URL!</span>', unsafe_allow_html=True)
     else:
-        webbrowser.open(f'https://nerinyan.moe/d/{extracted_string}')
         new_link = f'https://nerinyan.moe/d/{extracted_string}'
-        st.markdown('<span style="color: green;">Link converted to Nerinyan!</span>', unsafe_allow_html=True)
-        st.markdown(f'<span style="color: green;">{new_link}</span>', unsafe_allow_html=True)
+        st.markdown('<span style="color: yellow;">Link converted to Nerinyan!</span>', unsafe_allow_html=True)
+        st.markdown(f'<a href="{new_link}" target="_blank" style="color: green;">{new_link}</a>', unsafe_allow_html=True)
 
 if st.button('Chimu'):
     start_marker = "beatmapsets/"
@@ -29,16 +26,15 @@ if st.button('Chimu'):
     start_index = link.find(start_marker) + len(start_marker)
     end_index = link.find(end_marker)
     extracted_string = link[start_index:end_index]
-
+    
     if len(link) == 0:
         st.markdown('<span style="color: red;">Enter URL!</span>', unsafe_allow_html=True)
     elif 'beatmapsets/' not in link:
         st.markdown('<span style="color: red;">Invalid URL!</span>', unsafe_allow_html=True)
     else:
-        webbrowser.open(f'https://chimu.moe/d/{extracted_string}')
         new_link = f'https://chimu.moe/d/{extracted_string}'
-        st.markdown('<span style="color: green;">Link converted to Chimu!</span>', unsafe_allow_html=True)
-        st.markdown(f'<span style="color: green;">{new_link}</span>', unsafe_allow_html=True)
+        st.markdown('<span style="color: yellow;">Link converted to Chimu!</span>', unsafe_allow_html=True)
+        st.markdown(f'<a href="{new_link}" target="_blank" style="color: green;">{new_link}</a>', unsafe_allow_html=True)
 
 if st.button('BeatConnect'):
     start_marker = "beatmapsets/"
@@ -46,13 +42,12 @@ if st.button('BeatConnect'):
     start_index = link.find(start_marker) + len(start_marker)
     end_index = link.find(end_marker)
     extracted_string = link[start_index:end_index]
-
+    
     if len(link) == 0:
         st.markdown('<span style="color: red;">Enter URL!</span>', unsafe_allow_html=True)
     elif 'beatmapsets/' not in link:
         st.markdown('<span style="color: red;">Invalid URL!</span>', unsafe_allow_html=True)
     else:
-        webbrowser.open(f'https://beatconnect.io/b/{extracted_string}')
         new_link = f'https://beatconnect.io/b/{extracted_string}'
-        st.markdown('<span style="color: green;">Link converted to BeatConnect!</span>', unsafe_allow_html=True)
-        st.markdown(f'<span style="color: green;">{new_link}</span>', unsafe_allow_html=True)
+        st.markdown('<span style="color: yellow;">Link converted to BeatConnect!</span>', unsafe_allow_html=True)
+        st.markdown(f'<a href="{new_link}" target="_blank" style="color: green;">{new_link}</a>', unsafe_allow_html=True)
